b85bbe73543cc40ffb5e5f860962b0a59574ae96
salomonw <salomonw@bu.edu>
2020-05-06 13:41:41 -0400

    new results

--- experiments/run_experiments.py
+++ experiments/run_experiments.py
@@ -264,7 +264,7 @@ os.mkdir('results/'+ts)
 import data_analysis as DA
 
 # Specify file name
-fname = "data/200504COVID19MEXICO.csv"
+fname = "data/200505COVID19MEXICO.csv"
 
 feature_translate = {
         'SEXO': 'Gender',
