0543cd2b4d8544a1c922b089fdc72ee4ef66b86e
salomonw <salomonw@bu.edu>
2020-05-08 21:11:02 -0400

    update results

--- experiments/parameters.py
+++ experiments/parameters.py
@@ -51,7 +51,7 @@ def read_data(fname, y, percentage_train=0.7, additional_vars=False, waiting=Tru
                             #'FECHA_DEF',
                             #'INTUBADO',
                             #'NEUMONIA',
-                           'EDAD'
+                            #'EDAD'
                             #'NACIONALIDAD',
                             #'EMBARAZO',
                             #'HABLA_LENGUA_INDI',
@@ -90,6 +90,11 @@ def read_data(fname, y, percentage_train=0.7, additional_vars=False, waiting=Tru
         df['NEUMONIA'] = df['NEUMONIA'].replace(2,0)
 
 
+    age_groups = [i*5 for i in range(20)]
+    for i in range(len(age_groups)-1):
+        df[str(age_groups[i])+'-'+str(age_groups[i+1])] = 0
+        df[str(age_groups[i]) + '-' + str(age_groups[i + 1])] = d
+
     if additional_vars != False:
         for vars in additional_vars:
             CATEGORICAL_COLUMNS.append(vars)
@@ -125,163 +130,3 @@ def read_data(fname, y, percentage_train=0.7, additional_vars=False, waiting=Tru
 
 
     return [X_train, X_test, y_train, y_test, CATEGORICAL_COLUMNS, NUMERIC_COLUMNS]
-
-'''
-def read_data_eng(fname, y, percentage_train=0.7, additional_vars=False, waiting=True):
-    CATEGORICAL_COLUMNS = [#'FECHA_ACTUALIZACION',
-                           # 'ORIGEN',
-                           # 'SECTOR',
-                           # 'ENTIDAD_UM',
-                            'SEXO',
-                           # 'ENTIDAD_NAC',
-                           # 'ENTIDAD_RES',
-                           # 'MUNICIPIO_RES',
-                           # 'TIPO_PACIENTE',
-                            #'FECHA_INGRESO',
-                            #'FECHA_SINTOMAS',
-                            #'FECHA_DEF',
-                           # 'INTUBADO',
-                           # 'NEUMONIA',
-                            #'EDAD',
-                           # 'NACIONALIDAD',
-                            'EMBARAZO',
-                            #'HABLA_LENGUA_INDI',
-                            'DIABETES',
-                            'EPOC',
-                            'ASMA',
-                            'INMUSUPR',
-                            'HIPERTENSION',
-                            'OTRA_COM',
-                            'CARDIOVASCULAR',
-                            'OBESIDAD',
-                            'RENAL_CRONICA',
-                            'TABAQUISMO',
-                           # 'OTRO_CASO',
-                            #'RESULTADO',
-                           # 'MIGRANTE',
-                            #'PAIS_NACIONALIDAD',
-                           # 'PAIS_ORIGEN'
-                            #'UCI',
-                            ]
-    NUMERIC_COLUMNS = [     #'FECHA_ACTUALIZACION',
-                            #'ORIGEN',
-                            #'SECTOR',
-                            #'ENTIDAD_UM',
-                            #'SEXO',
-                            #'ENTIDAD_NAC',
-                            #'ENTIDAD_RES',
-                            #'MUNICIPIO_RES',
-                            #'TIPO_PACIENTE',
-                            #'FECHA_INGRESO',
-                            #'FECHA_SINTOMAS',
-                            #'FECHA_DEF',
-                            #'INTUBADO',
-                            #'NEUMONIA',
-                           'EDAD'
-                            #'NACIONALIDAD',
-                            #'EMBARAZO',
-                            #'HABLA_LENGUA_INDI',
-                            #'DIABETES',
-                            #'EPOC',
-                            #'ASMA',
-                            #'INMUSUPR',
-                            #'HIPERTENSION',
-                            #'OTRA_CON',
-                            #'CARDIOVASCULAR',
-                            #'OBESIDAD',
-                            #'RENAL_CRONICA',
-                            #'TABAQUISMO',
-                            #'OTRO_CASO',
-                            #'RESULTADO',
-                            #'MIGRANTE',
-                            #'PAIS_NACIONALIDAD',
-                            #'PAIS_ORIGEN',
-                            #'UCI',
-                            ]
-
-    df = pd.read_csv(fname, encoding="ISO-8859-1")
-
-    df = df[df['TIPO_PACIENTE'] < 3]
-    df['TIPO_PACIENTE'] = df['TIPO_PACIENTE'] - 1
-
-  
-    if y=='UCI':
-        CATEGORICAL_COLUMNS.append('NEUMONIA')
-        CATEGORICAL_COLUMNS.append('RESULTADO')
-        CATEGORICAL_COLUMNS.append('TIPO_PACIENTE')
-        df['UCI'] = df['UCI'].replace(97, 0)
-        df['UCI'] = df['UCI'].replace(98, 0)
-        df['UCI'] = df['UCI'].replace(99, 0)
-        df = df[df['UCI'] < 3]
-        df['UCI'] = df['UCI'].replace(2,0)
-
-    elif y=='INTUBADO':
-        CATEGORICAL_COLUMNS.append('NEUMONIA')
-        CATEGORICAL_COLUMNS.append('RESULTADO')
-        CATEGORICAL_COLUMNS.append('TIPO_PACIENTE')
-        df['INTUBADO'] = df['INTUBADO'].replace(97, 0)
-        df['INTUBADO'] = df['INTUBADO'].replace(98, 0)
-        df['INTUBADO'] = df['INTUBADO'].replace(99, 0)
-        df = df[df['INTUBADO'] < 3]
-        df['INTUBADO'] = df['INTUBADO'].replace(2,0)
- 
-    if additional_vars != False:
-        for vars in additional_vars:
-            CATEGORICAL_COLUMNS.append(vars)
-
-    df.FECHA_INGRESO = pd.to_datetime(df.FECHA_INGRESO)
-    df.FECHA_SINTOMAS = pd.to_datetime(df.FECHA_SINTOMAS)
-    df = df.replace("9999-99-99", "2022-04-05")
-    df.FECHA_DEF = pd.to_datetime(df.FECHA_DEF)
-    df['MUERTE'] = 0
-    df.loc[(df['FECHA_DEF']-df['FECHA_INGRESO']).dt.days <=100, 'MUERTE'] = 1
-
-    for i in CATEGORICAL_COLUMNS:
-        df[i] = df[i].replace(2,0)
-
-
-    cols = CATEGORICAL_COLUMNS.copy()
-    cols.extend(NUMERIC_COLUMNS)
-    cols.append(y)
-
-    if waiting == True:
-        df = df[(df['RESULTADO'] == 1) | (df['RESULTADO'] == 3)]
-    else:
-        df = df[(df['RESULTADO'] == 1)]
-
-    df = df[cols]
-    df.EDAD = df.EDAD/df.EDAD.max()
-
-    feature_translate = {
-        'SEXO': 'Gender',
-        'EMBARAZO': 'Pregnant',
-        'DIABETES': 'Diabetes',
-        'EPOC': 'COPD',
-        'ASMA': 'Asthma',
-        'INMUSUPR': 'Immunosuppression',
-        'HIPERTENSION': 'Hypertension',
-        'OTRA_COM': 'Other',
-        'CARDIOVASCULAR': 'Cardiovascular Disease',
-        'OBESIDAD': 'Obesity ',
-        'RENAL_CRONICA': 'Chronic Renal Insufficiency',
-        'TABAQUISMO': 'Tobacco Use',
-        'OTRO_CASO': 'Contact COVID',
-        'NEUMONIA': 'Pneumonia',
-        'INTUBADO': 'Ventilator',
-        'UCI': 'ICU',
-        'HABLA_LENGUA_INDI': 'Indigenous Len',
-        'TIPO_PACIENTE': 'Hospitalization',
-        'MUERTE': 'Death',
-        'RESULTADO': 'Test',
-        'EDAD': 'Age',
-        'COPD': 'Age'
-    }
-
-    df.rename(columns=feature_translate, inplace=True)
-    X_train = df.sample(frac=percentage_train)
-    X_test = pd.concat([df, X_train]).drop_duplicates(keep=False)
-    y_train = X_train.pop(feature_translate[y])
-    y_test = X_test.pop(feature_translate[y])
-
-    return [X_train, X_test, y_train, y_test, CATEGORICAL_COLUMNS, NUMERIC_COLUMNS]
-'''
---
+++ experiments/run_experiments_age_group.py
@@ -0,0 +1,114 @@
+import parameters as parameters
+import models as models
+import glob
+import os
+
+
+def run_model(y, fname, name, out_dir,  waiting, hosp=False,  add_vars=False, max_steps=10000):
+    if add_vars != False:
+        print(y + ' ' + name + " started! (" + str(len(add_vars)) + ")" )
+    else:
+        print(y + ' ' + name + " started! (1)")
+    if y=='deaths':
+        y_spanish = 'MUERTE'
+    elif y=='icu':
+        y_spanish = 'UCI'
+    elif y=='vent':
+        y_spanish = 'INTUBADO'
+    elif y=='hosp':
+        y_spanish = 'TIPO_PACIENTE'
+    elif y=='pneu':
+        y_spanish = 'NEUMONIA'
+
+    data = parameters.read_data_age_groups(fname=fname, y=y_spanish,
+                                percentage_train=0.7,
+                                additional_vars=add_vars,
+                                waiting=waiting,
+                                hosp=hosp)
+
+    [dftrain, dftest, y_train, y_test, CATEGORICAL_COLUMNS, NUMERIC_COLUMNS] = data
+
+
+    ## Feature elimination and dataset structure
+    dftrain, dftest, CATEGORICAL_COLUMNS = models.feature_elimination(dftrain, dftest, y_train, y_test, CATEGORICAL_COLUMNS, NUMERIC_COLUMNS, out_dir=out_dir, name=name)
+
+
+
+    res, coeff_LC = models.run_classification_experiment(dftrain, dftest, y_train, y_test,
+                                         CATEGORICAL_COLUMNS, NUMERIC_COLUMNS, out_dir=out_dir, name=name, max_steps=max_steps)
+
+    #coeff_LC.rename({'weights': name}, inplace=True)
+    coeffs = [coeff_LC]
+    return coeffs
+
+
+def run_exp_2(fname):
+    ## Run predictive models
+    out_dir = 'results/' + ts + '/PredictiveModels'
+    os.mkdir(out_dir)
+
+    coeff_LC = []
+
+    out_dir = 'results/' + ts + '/PredictiveModels/Hospitalizations'
+    os.mkdir(out_dir)
+    c = run_model(y='hosp', fname=fname, name='Y-W-0', out_dir=out_dir, waiting=False, hosp=False, add_vars=['RESULTADO'])
+    coeff_LC.extend(c)
+
+    out_dir = 'results/' + ts + '/PredictiveModels/Deaths'
+    os.mkdir(out_dir)
+    c = run_model(y='deaths', fname=fname, name='Y-W-0', out_dir=out_dir, waiting=True, hosp=True, add_vars=['RESULTADO'])
+    coeff_LC.extend(c)
+    c = run_model(y='deaths', fname=fname, name='Y-W-1', out_dir=out_dir, waiting=True, hosp=True, add_vars=['RESULTADO', 'NEUMONIA', 'INTUBADO', 'UCI'])
+    coeff_LC.extend(c)
+
+    out_dir = 'results/' + ts + '/PredictiveModels/ICU'
+    os.mkdir(out_dir)
+    c = run_model(y='icu', fname=fname, name='Y-W-0', out_dir=out_dir, waiting=True, hosp=True, add_vars=['RESULTADO'])
+    coeff_LC.extend(c)
+    c = run_model(y='icu', fname=fname, name='Y-W-1', out_dir=out_dir, waiting=True, hosp=True, add_vars=['RESULTADO', 'NEUMONIA'])
+    coeff_LC.extend(c)
+
+    out_dir = 'results/' + ts + '/PredictiveModels/Vent'
+    os.mkdir(out_dir)
+    c = run_model(y='vent', fname=fname, name='Y-W-0', out_dir=out_dir, waiting=True, hosp=True, add_vars=['RESULTADO'])
+    coeff_LC.extend(c)
+    c = run_model(y='vent', fname=fname, name='Y-W-1', out_dir=out_dir, waiting=True, hosp=True, add_vars=['RESULTADO', 'NEUMONIA', 'UCI'])
+    coeff_LC.extend(c)
+
+
+# Get timestamp
+ts_obj = datetime.now()
+ts = ts_obj.strftime("%d-%b-%Y_%H-%M-%S")
+os.mkdir('results/'+ts)
+
+
+# Specify file name
+fname = "data/200506COVID19MEXICO.csv"
+fname = max(glob.glob("data//*.csv") , key = os.path.getctime)
+
+feature_translate = {
+        'SEXO': 'Gender',
+        'EMBARAZO': 'Pregnant',
+        'DIABETES': 'Diabetes',
+        'EPOC': 'COPD',
+        'ASMA': 'Asthma',
+        'INMUSUPR': 'Immunosuppression',
+        'HIPERTENSION': 'Hypertension',
+        'OTRA_COM': 'Other',
+        'CARDIOVASCULAR': 'Cardiovascular Disease',
+        'OBESIDAD': 'Obesity ',
+        'RENAL_CRONICA': 'Chronic Renal Insufficiency',
+        'TABAQUISMO': 'Tobacco Use',
+        'OTRO_CASO': 'Contact COVID',
+        'NEUMONIA': 'Pneumonia',
+        'INTUBADO': 'Ventilator',
+        'UCI': 'ICU',
+        'HABLA_LENGUA_INDIG': 'Indigenous Len',
+        'TIPO_PACIENTE': 'Hospitalization',
+        'MUERTE': 'Death',
+        'RESULTADO': 'Test',
+        'EDAD': 'Age',
+        'COPD': 'Age'
+    }
+
+run_exp_2(fname)
